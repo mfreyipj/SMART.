a169463a50bc80b36052b28a030d082d45feef90
IsHaltEchtSo <d.grollmusz@gmail.com>
2020-10-08 22:03:49 +0200

    Update for admin

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Repo for the development of a Web-App to manage SMART goals
 The Goal of this Web Application is to give its user an intuitive tool that he can use 
 to track his SMART Goals. It should deliver graphical information about his progress and
 weekly/monthly feedback so that he knows how he's doing and if he needs to adjust.
+
+Optional Features:
+-Choose the level of feeling miserable after you interrupt a streak
--- progress_tracker/models.py
+++ progress_tracker/models.py
@@ -4,9 +4,27 @@ from django.db import models
 
 class Topic(models.Model):
     """A topic the user is learning about."""
-    text = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    detail_text = models.CharField(default="", max_length=100)
     date_added = models.DateTimeField(auto_now_add=True)
 
-def __str__(self):
-    """Return a string representation of the model.""" 
-    return self.text
+    def __str__(self):
+        """Return a string representation of the model.""" 
+        return self.title
+
+
+class Report(models.Model):
+    """Something specific learned about a topic."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Reports'
+
+    def __str__(self):
+        """Return a string representation of the model."""
+        if len(self.text) > 50:
+            return f"{self.text[:50]}..."
+        else:
+            return f"{self.text}"
